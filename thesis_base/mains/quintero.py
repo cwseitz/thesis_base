fa1798d51cb27c3b21d8be59950991ee20e1ee3d
cwseitz <cwseitz2@gmail.com>
2022-06-09 15:28:52 -0400

    update quintero

--- thesis_base/mains/quintero.py
+++ thesis_base/mains/quintero.py
@@ -6,13 +6,15 @@ import cellquant as cq
 import matplotlib.pyplot as plt
 from skimage.io import imread, imsave
 from skimage.util import img_as_ubyte
-from cellquant.smt import detect_blobs, detect_blobs_batch
+from cellquant.smt import detect_blobs, detect_blobs_batch, fit_psf, fit_psf_batch
 
 #read in the data
 path = '/home/cwseitz/Desktop/quintero_data/'
 file = 'FastImg_Cell01.tif'
 im = imread(path+file)
 nt, nx, ny, nc = im.shape
+test_im = im[:3,:,:,2]
 
 #detect RNA in all frames
-blobs_df, plt_array = detect_blobs_batch(im[:100,:,:,2],threshold=0.05)
+blobs_df, plt_array = detect_blobs_batch(test_im,threshold=0.05)
+psf_df, plt_array = fit_psf_batch(test_im,blobs_df,diagnostic=True,pltshow=True)
